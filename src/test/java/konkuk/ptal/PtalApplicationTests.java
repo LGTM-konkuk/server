fc937a4536955467afc5376ff5215fd50ca759b3
SIKU-KR <peter012677@naver.com>
2025-05-06 23:14:05 +0900

    initial commit

---
+++ src/test/java/konkuk/ptal/PtalApplicationTests.java
@@ -0,0 +1,13 @@
+package konkuk.ptal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
