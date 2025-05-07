51d33ab9febbb82a730b26c5fd3bdf3d98a624bc
jhmin99 <mjh012399@gmail.com>
2025-05-08 00:04:03 +0900

    feat: API 응답 관련 Dto 클래스 생성

---
+++ src/main/java/konkuk/ptal/dto/api/ErrorCode.java
@@ -0,0 +1,25 @@
+package konkuk.ptal.dto.api;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+    INVALID_JWT(4001, false, "유효하지 않은 토큰입니다."),
+    DUPLICATED_EMAIL(4002, false, "중복 이메일입니다."),
+    PASSWORD_NOT_AVAILABLE(4004, false, "사용할 수 없는 비밀번호입니다."),
+    PASSWORD_NOT_EQUAL(4005, false, "비밀번호가 일치하지 않습니다."),
+    EMAIL_NOT_AVAILABLE(4006, false, "사용할 수 없는 이메일입니다."),
+    USER_NOT_FOUND(4007, false, "사용자를 찾을 수 없습니다."),
+    ACCESS_DENIED(4008, false, "접근 권한이 없습니다."),
+    INTERNAL_SERVER_ERROR(5000, false, "서버 내부 에러.");
+
+    private final Integer code;
+    private final Boolean success;
+    private final String message;
+
+    ErrorCode(Integer code, Boolean success, String message) {
+        this.code = code;
+        this.success = success;
+        this.message = message;
+    }
+}
