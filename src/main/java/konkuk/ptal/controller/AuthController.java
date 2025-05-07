e70a7527e5eb01441bdc49c3663426c8c8ab1b2b
jhmin99 <mjh012399@gmail.com>
2025-05-08 00:03:33 +0900

    feat: 회원가입 및 로그인 기능 구현

---
+++ src/main/java/konkuk/ptal/controller/AuthController.java
@@ -0,0 +1,35 @@
+package konkuk.ptal.controller;
+
+import konkuk.ptal.dto.api.ApiResponse;
+import konkuk.ptal.dto.api.ResponseCode;
+import konkuk.ptal.dto.request.LoginRequestDto;
+import konkuk.ptal.dto.request.SignupRequestDto;
+import konkuk.ptal.dto.response.TokenResponseDto;
+import konkuk.ptal.service.IAuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+
+public class AuthController {
+
+    private final IAuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<Void>> signup(@RequestBody SignupRequestDto signupRequestDto) {
+        authService.register(signupRequestDto);
+        return ResponseEntity.ok(ApiResponse.success(ResponseCode.MEMBER_REGISTER_SUCCESS, null));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<TokenResponseDto>> login(@RequestBody LoginRequestDto loginRequestDto) {
+        TokenResponseDto token = authService.login(loginRequestDto);
+        return ResponseEntity.ok(ApiResponse.success(ResponseCode.LOGIN_SUCCESS, token));
+    }
+}
---
+++ src/main/java/konkuk/ptal/entity/User.java
@@ -0,0 +1,61 @@
+package konkuk.ptal.entity;
+
+import jakarta.persistence.*;
+import konkuk.ptal.domain.enums.Role;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, name = "password_hash")
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+    public static User createUser(String email, String passwordHash) {
+        return User.builder()
+                .email(email)
+                .passwordHash(passwordHash)
+                .role(Role.USER)
+                .build();
+    }
+
+    public void updateRole(Role newRole) {
+        this.role = newRole;
+        this.updatedAt = LocalDateTime.now();
+    }
+}
