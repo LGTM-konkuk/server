2f2f63c2ac6e1ee17a3c5517c77092ee4bbb53d8
Bumshik Park <peter012677@naver.com>
2025-05-10 12:10:12 +0900

    Reviewer Entity 정의

---
+++ src/main/java/konkuk/ptal/entity/Reviewer.java
@@ -0,0 +1,55 @@
+package konkuk.ptal.entity;
+
+import jakarta.persistence.*;
+import konkuk.ptal.util.StringListConverter;
+import lombok.*;
+import org.hibernate.annotations.Type;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "reviewers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Reviewer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String expertise;
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String bio;
+
+    @Convert(converter = StringListConverter.class)
+    @Column(columnDefinition = "TEXT")  // 태그 길이가 가변적일 수 있으니 TEXT 권장
+    private List<String> tags;
+
+    @Column(nullable = false, updatable = false)
+    LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
