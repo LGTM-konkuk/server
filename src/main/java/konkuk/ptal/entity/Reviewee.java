95c08fe7e9ab7f63f637bc90c858d09e85fc6596
Bumshik Park <peter012677@naver.com>
2025-05-22 11:24:59 +0900

    feat(reviewee): Add Reviewee entity
    This commit introduces the `Reviewee` entity.
    
    The `Reviewee` entity includes fields for:
    
    - ID
    - User (One-to-one relationship)
    - Display name
    - Preferences (List of strings)
    - Created at timestamp
    - Updated at timestamp

---
+++ src/main/java/konkuk/ptal/entity/Reviewee.java
@@ -0,0 +1,51 @@
+package konkuk.ptal.entity;
+
+import jakarta.persistence.*;
+import konkuk.ptal.util.StringListConverter;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name="reviewees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Reviewee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false, name="display_name")
+    private String displayName;
+
+    @Convert(converter = StringListConverter.class)
+    @Column(columnDefinition = "TEXT")
+    private List<String> preferences;
+
+    @Column(nullable = false, updatable = false)
+    LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
